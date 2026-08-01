1f47d76db2fe86ccc186dc6a49a4209f9f0dc8f2
safeswing <safe22183@gmail.com>
2026-08-01 13:09:50 +0500

    More detailed errors and adding error codes.

--- bblaunch/bblaunch.cpp
+++ bblaunch/bblaunch.cpp
@@ -1,84 +1,105 @@
-
-#include <string>
 #include <windows.h>
+#include <string>
 
-//#include "checkdx.h"
-//#include "checkie.h"
+using std::string;
 
-using namespace std;
+// Static Error Messages
+static const char* md_err = "Your desktop must be in high-colour mode (at least 16-bit) to use Blitz Basic.\n\n"
+"You can change your display settings from the Control Panel.";
 
-/*
-static const char *dx_err=
-"You must have DirectX version 7 installed to run Blitz Basic.\n\n"
-"DirectX 7 is provided on the Blitz Basic CD in the DirectX7 folder.\n\n"
-"The latest version of DirectX is available from www.microsoft.com";
+// Converts Win32 API system error numbers into human-readable text
+static string getSystemErrorMessage(DWORD errorCode) {
+    if (errorCode == 0) return "No system error reported.";
 
-static const char *ie_err=
-"You must have Internet Explorer version 4 installed to run Blitz Basic.\n\n"
-"Internet Explorer 5 is provided on the Blitz Basic CD in the IExplorer5 folder.\n\n"
-"The latest version of Internet Explorer is available from www.microsoft.com";
-*/
+    LPSTR messageBuffer = nullptr;
+    size_t size = FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        errorCode,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPSTR)&messageBuffer,
+        0,
+        NULL
+    );
 
-static const char *bb_err=
-"Unable to run Blitz Basic";
-
-static const char *md_err=
-"Your desktop must be in high-colour mode to use Blitz Basic.\n\n"
-"You can change your display settings from the control panel."; 
-
-static string getAppDir(){
-	char buff[MAX_PATH];
-	if( GetModuleFileName( 0,buff,MAX_PATH ) ){
-		string t=buff;
-		int n=t.find_last_of( '\\' );
-		if( n!=string::npos ) t=t.substr( 0,n );
-		return t;
-	}
-	return "";
+    string message = messageBuffer ? string(messageBuffer, size) : "Unknown system error.";
+    LocalFree(messageBuffer);
+    return message;
 }
 
-static void fail( const char *p ){
-	::MessageBox( 0,p,"Blitz Basic Error",MB_SETFOREGROUND|MB_TOPMOST|MB_ICONERROR );
-	ExitProcess(-1);
+// Display error modal with custom error code format
+static void fail(const string& customErrorCode, const string& description) {
+    string fullMsg = "Blitz Basic Launcher Error\n\n" +
+        string("Error Code: ") + customErrorCode + "\n" +
+        "Details: " + description;
+
+    ::MessageBoxA(0, fullMsg.c_str(), "Blitz Basic Error", MB_SETFOREGROUND | MB_TOPMOST | MB_ICONERROR);
+    ExitProcess(-1);
 }
 
-static int desktopDepth(){
-	HDC hdc=GetDC( GetDesktopWindow() );
-	return GetDeviceCaps( hdc,BITSPIXEL );
+static string getAppDir() {
+    char buff[MAX_PATH];
+    if (GetModuleFileNameA(0, buff, MAX_PATH)) {
+        string t = buff;
+        int n = t.find_last_of('\\');
+        if (n != string::npos) t = t.substr(0, n);
+        return t;
+    }
+    return "";
 }
 
-int _stdcall WinMain( HINSTANCE inst,HINSTANCE prev,char *cmd,int show ){
+static int desktopDepth() {
+    HDC hdc = GetDC(GetDesktopWindow());
+    int depth = GetDeviceCaps(hdc, BITSPIXEL);
+    ReleaseDC(GetDesktopWindow(), hdc);
+    return depth;
+}
 
-	if( desktopDepth()<16 ) fail( md_err );
+int _stdcall WinMain(HINSTANCE inst, HINSTANCE prev, char* cmd, int show) {
 
-	/*
-#ifndef PLUS
-	if( getDXVersion()<7 ) fail( dx_err );
-#endif
+    // Check display color depth
+    if (desktopDepth() < 16) {
+        fail("ERR_0x5X95_01", md_err);
+    }
 
-	if( getIEVersion()<4 ) fail( ie_err );
-	*/
+    // Determine current directory
+    string appDir = getAppDir();
+    if (appDir.empty()) {
+        fail("ERR_0x5X95_02", "Failed to resolve application root directory.");
+    }
 
-	//Ugly hack to get application dir...
-	string t=getAppDir();
-	putenv( ("blitzpath="+t).c_str() );
-	SetCurrentDirectory( t.c_str() );
-	t=t+"\\bin\\ide.exe "+cmd;
+    // Set environment variable and working path
+    putenv(("blitzpath=" + appDir).c_str());
+    if (!SetCurrentDirectoryA(appDir.c_str())) {
+        DWORD sysErr = GetLastError();
+        fail("ERR_0x5X95_03", "Could not set working directory to: " + appDir + "\nSystem Message: " + getSystemErrorMessage(sysErr));
+    }
 
-    STARTUPINFO si;
+    // Target executable path
+    string idePath = appDir + "\\bin\\ide.exe";
+    string commandLine = idePath + " " + cmd;
+
+    STARTUPINFOA si;
     PROCESS_INFORMATION pi;
-    ZeroMemory(&si,sizeof(si));si.cb=sizeof(si);
-	if( !CreateProcess( 0,(char*)t.c_str(),0,0,0,0,0,0,&si,&pi ) ){
-		::MessageBox( 0,bb_err,"Blitz Basic Error",MB_SETFOREGROUND|MB_TOPMOST|MB_ICONERROR );
-		ExitProcess(-1);
-	}
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
 
-	//wait for BB to start
-	WaitForInputIdle( pi.hProcess,INFINITE );
+    // Launch the Blitz IDE
+    if (!CreateProcessA(0, (char*)commandLine.c_str(), 0, 0, 0, 0, 0, 0, &si, &pi)) {
+        DWORD sysErr = GetLastError();
+        string errDetails = "Failed to launch main IDE executable.\n" +
+            string("Target Path: ") + idePath + "\n" +
+            "System Error (" + std::to_string(sysErr) + "): " + getSystemErrorMessage(sysErr);
 
-    // Close process and thread handles. 
-    CloseHandle( pi.hProcess );
-    CloseHandle( pi.hThread );
+        fail("ERR_0x5X95_04", errDetails);
+    }
 
-	return 0;
+    // Wait for Blitz Basic to initialize before terminating launcher
+    WaitForInputIdle(pi.hProcess, INFINITE);
+
+    // Clean up handle allocations
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
 }
